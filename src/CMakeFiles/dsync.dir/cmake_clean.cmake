e0f28645222a646e80f7bd5f4077474ab21a818c
Daanish Suhail <theczaar@gmail.com>
2019-02-10 09:08:05 -0600

    added cmake

---
+++ src/CMakeFiles/dsync.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsync.dir/main.cpp.o"
+  "dsync.pdb"
+  "dsync"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dsync.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
